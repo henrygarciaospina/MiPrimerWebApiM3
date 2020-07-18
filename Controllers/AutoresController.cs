f412151c9a7896886f26b3e2f6497128db69769d
HegaRo <henrygarciaospina@gmail.com>
2020-07-17 23:12:27 -0500

    Api completa y funcional

---
+++ Controllers/AutoresController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MiPrimerWebApiM3.Contexts;
+using MiPrimerWebApiM3.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiPrimerWebApiM3.Controllers
+{
+    // api/autores
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context; 
+        }
+
+        // GET api/autores
+        [HttpGet]
+        public ActionResult<IEnumerable<Autor>> Get()
+        {
+            return context.Autores.Include(l => l.Libros).ToList();
+        }
+
+        // GET api/autores/primer
+        [HttpGet("primer")]
+        public ActionResult<Autor> GetPrimerAutor()
+        {
+            return context.Autores
+                   .Include(l => l.Libros)
+                   .FirstOrDefault();
+        }
+
+        // GET api/autores/1
+        [HttpGet("{id}", Name = "ObtenerAutor")]
+        public async Task<ActionResult<Autor>> Get(int id) 
+        {
+            var autor = await context.Autores.Include(l => l.Libros).FirstOrDefaultAsync(a => a.Id == id);
+
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            return autor;
+        }
+
+        // POST api/autores
+        [HttpPost]
+        public ActionResult Post([FromBody] Autor autor)
+        {
+            context.Autores.Add(autor);
+            context.SaveChanges();
+            return new CreatedAtRouteResult("ObtenerAutor", new { id = autor.Id }, autor);
+        }
+
+        // PUT api/autores/1
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [ FromBody] Autor value)
+        {
+            if (id != value.Id)
+            {
+                return BadRequest();
+            }
+            context.Entry(value).State = EntityState.Modified;
+            context.SaveChanges();
+            return Ok();
+        }
+
+        // DELETE api/autores/1
+        [HttpDelete("{id}")]
+        public ActionResult <Autor> Delete(int id)
+        {
+            var autor = context.Autores.FirstOrDefault(a => a.Id == id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            context.Autores.Remove(autor);
+            context.SaveChanges();
+            return autor;
+        }
+    }
+}
---
+++ Controllers/LibrosController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MiPrimerWebApiM3.Contexts;
+using MiPrimerWebApiM3.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MiPrimerWebApiM3.Controllers
+{
+    // api/libros
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibrosController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public LibrosController(ApplicationDbContext context) 
+        {
+            this.context = context;
+        }
+
+        // GET api/libros
+        [HttpGet]
+        public ActionResult<IEnumerable<Libro>> Get()
+        {
+            return context.Libros.Include(a => a.Autor).ToList();
+        }
+
+        // GET api/libros/1
+        [HttpGet("{id}", Name = "ObtenerLibro")]
+        public ActionResult<Libro> Get(int id)
+        {
+            var libro = context.Libros.FirstOrDefault(l => l.Id == id);
+
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return libro;
+        }
+
+        // POST api/libros
+        [HttpPost]
+        public ActionResult Post([FromBody] Libro libro)
+        {
+            context.Libros.Add(libro);
+            context.SaveChanges();
+            return new CreatedAtRouteResult("ObtenerLibro", new { id = libro.Id }, libro);
+        }
+
+        // PUT api/libros/1
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Libro value)
+        {
+            if (id != value.Id)
+            {
+                return BadRequest();
+            }
+            context.Entry(value).State = EntityState.Modified;
+            context.SaveChanges();
+            return Ok();
+        }
+
+
+        // DELETE api/libros/1
+        [HttpDelete("{id}")]
+        public ActionResult<Libro> Delete(int id)
+        {
+            var libro = context.Libros.FirstOrDefault(l => l.Id == id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+            context.Libros.Remove(libro);
+            context.SaveChanges();
+            return libro;
+        }
+    }
+}
